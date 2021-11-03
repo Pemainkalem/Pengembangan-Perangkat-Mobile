70f48cc615a1e04a5a5e48de963ed9e5746d6b83
Pemainkalem <58874774+Pemainkalem@users.noreply.github.com>
2021-11-03 22:52:18 +0700

    Add files via upload

---
+++ Tugas5/no1.js
@@ -0,0 +1,18 @@
+//menyusun array berdasarkan abjad
+let buah = [ 'Apel', 'Pisang', 'Jambu', 'Anggur', 'Pepaya', 'Duku', 'Durian', 'Jeruk', 'Mangga', 'Manggis'];
+buah.sort(); 
+console.log(buah);
+
+//mengemlompokan array
+console.log("Pengelompokan buah");
+let A = ['Apel']
+let D = ['Duku']
+let J = ['Jambu']
+let M = ['Mangga']
+let P = ['Pepaya']
+A.push('Anggur');
+D.push('Durian');
+J.push('Jeruk');
+M.push('Manggis');
+P.push('Pisang');
+console.log(A,D,J,M,P);
